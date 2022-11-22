b02efb568fc9784070a89e3de7a1d0e443797b9f
saptarshi chakraborty <saptarshi34.sc@gmail.com>
2022-11-22 18:03:50 +0530

    add bootstrap class and add image

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 
 const express = require('express')
+const path = require('path')
 const stripe = require('stripe')('sk_test_51H32d8DcLa5pUV8grTcV3vj3r7xK9Yac5iahH1Og1KQNtTmxXa1B753EHW9dK1YdzeKxdLrwWX6OkmXMLpLA1Jtn00KcrTAdN9')
 const bodyParser = require('body-parser')
-const handlebars= require('express-handlebars')
+const hbars= require('express-handlebars')
 
 const app = express()
+//middlewar//
 
 //handelbars middleware
-app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
+
+app.engine('handlebars', hbars.engine({ 
+    defaultLayout: 'index',
+    layoutsDir: __dirname + '/views/layouts'
+ }));
 //app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+//app.set('views', path.join(__dirname, 'views'))
 
 
 //body parser middleware
@@ -21,7 +28,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(`${__dirname}/public`))
 //index Route::
 app.get('/',(req,res)=>{
-res.render('index',{layout: false})
+res.render('main',{layout: 'index'})
 })
 
 const port = process.env.PORT || 5000
